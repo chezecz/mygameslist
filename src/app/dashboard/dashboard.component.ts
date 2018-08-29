8983652c5de6d1a848b7ac67f8bbbf035f7896bb
DezeG <gromovdeze@yandex.ru>
2018-08-29 20:37:56 +1000

    Style update

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -3,20 +3,19 @@ import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
+	constructor(
+		private route: ActivatedRoute,
+		private location: Location
+	) { }
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location
-    ) { }
+	title = "MyGameList";
 
-  title = "MyGameList"
-
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
 }
