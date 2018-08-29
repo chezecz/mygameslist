d50d0f93d3bef402142b5426b1162889a6231163
chezecz <cheze92@gmail.com>
2018-08-29 19:38:15 +1000

    fix structure

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Game } from '../game';
+import { Game } from '../../classes/game';
 
 @Component({
   selector: 'app-description',
--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -2,8 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Location } from '@angular/common';
 
-import { Game } from '../game';
-import { User } from '../user';
+import { Game } from '../../classes/game';
+import { User } from '../../classes/user';
 
 @Component({
   selector: 'app-user',
