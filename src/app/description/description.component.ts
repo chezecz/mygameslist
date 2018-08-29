dcdb56ba338b42a4965c62b6572247e9456e0cac
DezeG <gromovdeze@yandex.ru>
2018-08-29 20:39:27 +1000

    Style update

--- src/app/description/description.component.ts
+++ src/app/description/description.component.ts
@@ -5,47 +5,47 @@ import { Location } from '@angular/common';
 import { Game } from '../../classes/game';
 
 @Component({
-  selector: 'app-description',
-  templateUrl: './description.component.html',
-  styleUrls: ['./description.component.css']
-})
+	selector: 'app-description',
+	templateUrl: './description.component.html',
+	styleUrls: ['./description.component.css']
+});
 export class DescriptionComponent implements OnInit {
 
-  games : Game[] = [
-  {
-    id: 1, name: "Mario Odyssey", description: "Platformer & Adventure"
-  },
-  {
-    id: 2, name: "Breath of the Wild", description: "Adventure"
-  },
-  {
-    id: 3, name: "Skyrim", description: "RPG Adventure"
-  },
-  {
-    id: 4, name: "Octopath Traveler", description: "JRPG"
-  },
-  {
-    id: 5, name: "Dead Cells", description: "Rogue-lite"
-  },
-  {
-    id: 6, name: "Sonic Mania", description: "2D Platformer"
-  },
-  {
-    id: 7, name: "Hollow Knight", description: "Metroidvania"
-  }]
+	games : Game[] = [
+	{
+		id: 1, name: "Mario Odyssey", description: "Platformer & Adventure"
+	},
+	{
+		id: 2, name: "Breath of the Wild", description: "Adventure"
+	},
+	{
+		id: 3, name: "Skyrim", description: "RPG Adventure"
+	},
+	{
+		id: 4, name: "Octopath Traveler", description: "JRPG"
+	},
+	{
+		id: 5, name: "Dead Cells", description: "Rogue-lite"
+	},
+	{
+		id: 6, name: "Sonic Mania", description: "2D Platformer"
+	},
+	{
+		id: 7, name: "Hollow Knight", description: "Metroidvania"
+	}]
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location
-    ) { }
+	constructor(
+		private route: ActivatedRoute,
+		private location: Location
+		) { }
 
-  title = "Description"
+	title = "Description";
 
-  ngOnInit() {
-  }
+	ngOnInit() {
+	}
 
-  back(): void {
-    this.location.back();
-  }
+	back(): void {
+		this.location.back();
+	}
 
 }
