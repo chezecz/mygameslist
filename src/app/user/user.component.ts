addc56239d94f76ca4c87c615977a03c298b0bca
DezeG <gromovdeze@yandex.ru>
2018-08-29 20:40:54 +1000

    Style update

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -6,61 +6,61 @@ import { Game } from '../../classes/game';
 import { User } from '../../classes/user';
 
 @Component({
-  selector: 'app-user',
-  templateUrl: './user.component.html',
-  styleUrls: ['./user.component.css']
-})
+	selector: 'app-user',
+	templateUrl: './user.component.html',
+	styleUrls: ['./user.component.css']
+});
 export class UserComponent implements OnInit {
 
-  constructor(
-    private route: ActivatedRoute,
-    private location: Location
-    ) {}
+	constructor(
+		private route: ActivatedRoute,
+		private location: Location
+		) {}
 
-  user : User[] = [
-  {
-  	id: 1, name: "EternalDeze"
-  },
-  {
-  	id: 2, name: "KKomrade"
-  },
-  {
-  	id: 3, name: "NintendoNumberOneFan"
-  }]
+	user: User[] = [
+	{
+		id: 1, name: "EternalDeze"
+	},
+	{
+		id: 2, name: "KKomrade"
+	},
+	{
+		id: 3, name: "NintendoNumberOneFan"
+	}]
 
-  games : Game[] = [
-  {
-  	id: 1, name: "Mario Odyssey", description: "Platformer & Adventure"
-  },
-  {
-  	id: 2, name: "Breath of the Wild", description: "Adventure"
-  },
-  {
-  	id: 3, name: "Skyrim", description: "RPG Adventure"
-  },
-  {
-  	id: 4, name: "Octopath Traveler", description: "JRPG"
-  },
-  {
-  	id: 5, name: "Dead Cells", description: "Rogue-lite"
-  },
-  {
-  	id: 6, name: "Sonic Mania", description: "2D Platformer"
-  },
-  {
-  	id: 7, name: "Hollow Knight", description: "Metroidvania"
-  }]
+	games: Game[] = [
+	{
+		id: 1, name: "Mario Odyssey", description: "Platformer & Adventure"
+	},
+	{
+		id: 2, name: "Breath of the Wild", description: "Adventure"
+	},
+	{
+		id: 3, name: "Skyrim", description: "RPG Adventure"
+	},
+	{
+		id: 4, name: "Octopath Traveler", description: "JRPG"
+	},
+	{
+		id: 5, name: "Dead Cells", description: "Rogue-lite"
+	},
+	{
+		id: 6, name: "Sonic Mania", description: "2D Platformer"
+	},
+	{
+		id: 7, name: "Hollow Knight", description: "Metroidvania"
+	}]
 
-  ngOnInit() {
-    this.getUser();
+	ngOnInit() {
+		this.getUser();
 }
 
-  getUser(): void {
-    const id = +this.route.snapshot.paramMap.get('id');
-  }
+	getUser(): void {
+		const id = +this.route.snapshot.paramMap.get('id');
+	}
 
-  back(): void {
-    this.location.back();
-  }
+	back(): void {
+		this.location.back();
+	}
 
 }
