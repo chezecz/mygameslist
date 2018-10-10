d6d5cb4be5a29e4fc017f1a22d6d7a00b2bc5bf3
chezecz <cheze92@gmail.com>
2018-10-10 16:59:46 +1100

    connect with gcloud storage

--- backend/env.js
+++ backend/env.js
@@ -1,12 +1,24 @@
-const fs = require('fs')
-const dotEnvExists = fs.existsSync('.env')
-const storage = require('@google-cloud/storage')
-const bucketStorage = `envvars${process.env.PROJECT_NAME}`
+'use strict'
 
-storage
-  .bucket(bucketStorage)
+const fs = require('fs')
+
+const dotEnvExists = fs.existsSync('.env')
+if (dotEnvExists) {
+  console.log('getEnv.js: .env exists, probably running on development environment')
+  process.exit()
+}
+
+const gcs = require('@google-cloud/storage')()
+
+const bucketName = `envvars${process.env.PROJECT_NAME}`
+console.log(`Downloading .env from bucket "${bucketName}"`)
+gcs
+  .bucket(bucketName)
   .file('.env')
   .download({ destination: '.env' })
   .then(() => {
     console.info('getEnv.js: .env downloaded successfully')
+  })
+  .catch(e => {
+    console.error(`getEnv.js: There was an error: ${JSON.stringify(e, undefined, 2)}`)
   })
